ea6f77d50c64c95f67a56e3555bd70ba837abb97
Paul Breen <pbree@bas.ac.uk>
2018-08-20 17:40:22 +0100

    Bumped the version number for the display formatting feature

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='data-mimic',
-      version='0.4',
+      version='0.5',
       description='Data Mimic',
       url='https://github.com/paul-breen/data-mimic',
       author='Paul Breen',
