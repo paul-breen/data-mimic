e5b40c315037f0db86093e7b978721f45041ca5a
Paul Breen <pbree@bas.ac.uk>
2018-08-20 09:36:41 +0100

    Bumped the version number to account for the new PLC Data API class

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='data-mimic',
-      version='0.3',
+      version='0.4',
       description='Data Mimic',
       url='https://github.com/paul-breen/data-mimic',
       author='Paul Breen',
